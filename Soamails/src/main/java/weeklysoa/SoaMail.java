3ae66bb8389367e37fb6b722fb535244ed05cd17
admin <admin@DESKTOP-C14LOAJ>
2022-07-17 22:03:22 +0530

    mail added

--- Soamails/src/main/java/weeklysoa/SoaMail.java
+++ Soamails/src/main/java/weeklysoa/SoaMail.java
@@ -2,27 +2,12 @@ package weeklysoa;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-import javax.mail.Authenticator;
-import javax.mail.BodyPart;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
-import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -32,9 +17,41 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class SoaMail {
 
-	public static void main(String[] args) {
-		System.out.println("Thank you sir");
-		System.out.println("First comments");
+	public static void main(String[] args) throws IOException {
+		Map<Integer, String> add1 = new HashMap<Integer, String>();
+		Map<Integer, String> add2 = new HashMap<Integer, String>();
+		FileInputStream xlsfile = new FileInputStream("e:/debtors/Master.xlsx");
+		Workbook xls = WorkbookFactory.create(xlsfile);
+		Sheet sht = xls.getSheetAt(2);
+		for (Row r : sht) {
+			if (r.getRowNum() > 0) {
+				add1.put((int) r.getCell(0).getNumericCellValue(), r.getCell(2).getStringCellValue());
+				add2.put((int) r.getCell(0).getNumericCellValue(), r.getCell(3).getStringCellValue());
+			}
+		}
+		xlsfile.close();
+		int code = 0;
+		File[] soa = new File("e://mails/").listFiles();
+		for (File s : soa) {
+			PDDocument soamail = Loader.loadPDF(s);
+			{
+				PDFTextStripper soatext = new PDFTextStripper();
+				soatext.setStartPage(1);
+				soatext.setEndPage(1);
+				String pdfFileInText = soatext.getText(soamail);
+				String lines[] = pdfFileInText.split("\\r?\\n");
+				code = Integer.parseInt(lines[4].substring(lines[4].indexOf(":") + 2));
+
+				System.out.println(lines[4].substring(lines[4].indexOf(":") + 2) + " ~ "
+						+ lines[5].substring(lines[5].indexOf(":") + 2) + " ~ "
+						+ lines[16].substring(lines[16].indexOf(":") + 2, 43) + " ~ " + add1.get(code) + " ~ "
+						+ add2.get(code));
+
+			}
+		}
+	
+		System.out.println("Dhandapani ");
+		System.out.println("Ok");
 	}
 
 }
